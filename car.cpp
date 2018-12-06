30b2f091569d45ca6a79417736eacda03a67d312
MaxiPineker <maxi.pineker@gmail.com>
2018-12-06 13:34:19 +0100

    tests running

--- car.cpp
+++ car.cpp
@@ -1,14 +1,37 @@
 #include "car.h"
+#include <stdbool.h>
+
 
 struct CarImplementation{
+  enum Color color;
+  enum CarType type;
+  bool is_rented;
+  int max_speed;
+  double fill_level;
+  double acceleration_rate;
   int speed;
-  double fuel_level;
-  double accelerate;
+  double lowest_acceleration_rate;
+  double highest_acceleration_rate;
 };
+
+
+struct CarImplementation aixam1={RED, AIXAM, false, 45,16.0,0.0,0,-8.0,1.0};
+struct CarImplementation fiat_multipla1={GREEN, FIAT_MULTIPLA, false, 170,65.0,0.0,0,-8.0,2.26};
+struct CarImplementation fiat_multipla2={BLUE, FIAT_MULTIPLA, false, 170,65.0,0.0,0,-8.0,2.26};
+struct CarImplementation fiat_multipla3={ORANGE, FIAT_MULTIPLA, false, 170,65.0,0.0,0,-8.0,2.26};
+struct CarImplementation jeep1={SILVER, JEEP, false, 196,80.0,0.0,0,-8.0,3.14};
+struct CarImplementation jeep2={BLACK, JEEP, false, 196,80.0,0.0,0,-8.0,3.14};
+
+Car car_park[]={&aixam1,&fiat_multipla1,&fiat_multipla2,&fiat_multipla3,&jeep1,&jeep2};
 
 Car get_car(enum CarType type)
 {
-  return 0;
+  for (int i = 0; i < 6; i++) {
+    if ((type == car_park[i]->CarImplementation->type)&&(car_park[i].)) {
+      CarImplementation->is_rented = true;
+      return
+    }
+  }
 }
 enum CarType get_type(Car car)
 {
