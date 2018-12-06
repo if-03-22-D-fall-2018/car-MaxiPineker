1ec5460eccf2305cccf88649d680d412b1ed8d23
MaxiPineker <maxi.pineker@gmail.com>
2018-12-06 11:41:42 +0100

    file.h geschafft

---
+++ car.cpp
@@ -0,0 +1,44 @@
+#include "car.h"
+
+struct CarImplementation{
+  int speed;
+  double fuel_level;
+  double accelerate;
+};
+
+Car get_car(enum CarType type)
+{
+  return 0;
+}
+enum CarType get_type(Car car)
+{
+  return AIXAM;
+}
+enum Color get_color(Car car)
+{
+  return RED;
+}
+double get_fill_level(Car car)
+{
+  return 0;
+}
+double get_acceleration_rate(Car car)
+{
+  return 0;
+}
+int get_speed(Car car)
+{
+  return 0;
+}
+void accelerate(Car car)
+{
+
+}
+void init()
+{
+
+}
+void set_acceleration_rate(Car car,double acceleration)
+{
+
+}
--- car.h
+++ car.h
@@ -13,4 +13,23 @@
 #ifndef ___CAR_H
 #define ___CAR_H
 
+typedef struct CarImplementation* Car;
+
+enum CarType { AIXAM, FIAT_MULTIPLA, JEEP };
+enum Color { RED, GREEN, BLUE, ORANGE, SILVER, BLACK };
+
+Car get_car(enum CarType type);
+
+enum CarType get_type(Car car);
+enum Color get_color(Car car);
+
+double get_fill_level(Car car);
+double get_acceleration_rate(Car car);
+
+int get_speed(Car car);
+void accelerate(Car car);
+void init();
+void set_acceleration_rate(Car car,double acceleration);
+
+
 #endif
